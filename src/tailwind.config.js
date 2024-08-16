db7cc3a58dd158beae5f00b571899e6a36702dcf
sandhyaananthan121 <sandhyaananthan@ufl.edu>
2024-08-16 01:58:05 -0400

    sandhya - new changes

---
+++ src/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        'custom-width': '1527px',
+      },
+      maxWidth: {
+        '1634': '1527px',
+      },
+    },
+  },
+  plugins: [],
+}
